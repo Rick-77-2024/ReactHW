3631e3cee2a15ba5850276f7314bd3de6fc18d49
rick-77-2024 <rick772006@gmail.com>
2025-08-02 10:45:46 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TodoComponents from "./components/todo-components/TodoComponents.tsx";
+
+function App() {
+
+
+  return (
+    <div>
+      <TodoComponents/>
+    </div>
+  )
+}
+
+export default App

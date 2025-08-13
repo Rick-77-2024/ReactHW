473dd3c0925a0ed14c013dddc1c7f0eaa0d8f956
rick-77-2024 <rick772006@gmail.com>
2025-08-13 16:36:47 +0300

    Final commit hw8.1

---
+++ src/routes/router.tsx
@@ -0,0 +1,20 @@
+
+import {createBrowserRouter, type RouteObject} from "react-router-dom";
+import {MainLayout} from "../layouts/MainLayout.tsx";
+import {UsersPage} from "../pages/UsersPage.tsx";
+import {PostsPage} from "../pages/PostsPage.tsx";
+import {UserPage} from "../pages/UserPage.tsx";
+import {CommentsPage} from "../pages/CommentsPage.tsx";
+
+const routes: RouteObject[] = [
+    {
+        path: '', element: <MainLayout/>, children: [
+            {path: 'users', element: <UsersPage/>},
+            {path: 'users/:id', element: <UserPage/>},
+            {path: 'posts', element: <PostsPage/>},
+            {path: 'comments', element: <CommentsPage/>},
+        ]
+    }
+
+];
+export const router = createBrowserRouter(routes);

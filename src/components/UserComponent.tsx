b9138277bdb6c7a5572f841e9f371f70e3644f38
rick-77-2024 <rick772006@gmail.com>
2025-08-13 16:33:33 +0300

    Final commit hw7.2

---
+++ src/components/UserComponent.tsx
@@ -0,0 +1,14 @@
+
+import {type IUser} from "../models/IUser.ts";
+
+interface UserComponentProps {
+    user: IUser
+}
+
+export const UserComponent = ({user}: UserComponentProps) => {
+    return (
+        <div>
+            <p>{user.id}: {user.name}</p>
+        </div>
+    );
+};
